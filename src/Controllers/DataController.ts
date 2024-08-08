5982721e899d166634abc7d645f8061948a3e64f
Eloy <eloymjunior00@gmail.com>
2024-08-08 18:59:51 -0300

    General

--- src/Controllers/DataController.ts
+++ src/Controllers/DataController.ts
@@ -147,9 +147,9 @@ class DataController {
                     data: [{ label: "Venda de Combustível", value: Math.round(sumFuel * 100) / 100, secondary_label: "TMC", secondary_value: Math.round((sumFuel / quantSupply) * 100) / 100 },
                     { label: "Volume Total", value: Math.round(sumLiterage * 100) / 100, secondary_label: "TMV", secondary_value: Math.round((sumLiterage / quantSupply) * 100) / 100 },
                     { label: "Venda de Produto", value: Math.round(sumFuelProd * 100) / 100, secondary_label: "TMP", secondary_value: Math.round((sumFuelProd / quantSupply) * 100) / 100 },
-                    { label: "Quantidade de Produto Vendido", value: Math.round(sumLiterageProd * 100) / 100, secondary_label: "TMP", secondary_value: sumLiterageProd / quantSupply },
-                    { label: "Venda de Serviços", value: 2000, secondary_label: "TMS", secondary_value: 0.54 },
-                    { label: "Outros", value: 50000, secondary_label: "TMS", secondary_value: 0.89 },
+                    { label: "Quantidade de Produto Vendido", value: Math.round(sumLiterageProd * 100) / 100},
+                    { label: "Venda de Serviços", value: 2000},
+                    { label: "Outros", value: 50000 },
                     ]
                 })
             } else {
